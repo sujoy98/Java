36d24ab4eddfa08a766c5b89c7d4e0c1f56e8164
sujoy98 <32317613+sujoy98@users.noreply.github.com>
2020-06-06 21:25:58 +0530

    Add files via upload

---
+++ Patt15.java
@@ -0,0 +1,17 @@
+/*
+A 
+A B 
+A B C 
+A B C D 
+A B C D E
+ */
+public class Patt15{
+    public static void main(String[]args){
+        for(char i='A';i<='E';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j + " ");
+            }
+        System.out.println();
+        }
+    }
+}
