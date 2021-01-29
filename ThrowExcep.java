e261df5ed11586d6dbf221a390071b82c850c754
sujoy98 <sujoysarkar951@gmail.com>
2021-01-29 19:55:32 +0530

    u

---
+++ ThrowExcep.java
@@ -0,0 +1,12 @@
+public class ThrowExcep {
+    public static void main(String[] args) {
+        int age=15;
+        try {
+            if(age<18) {
+                throw new ArithmeticException("Age less than 18");
+            }
+        } catch(ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
