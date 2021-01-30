d36cf2637d48bcad2058769d5379836917cf516d
sujoy98 <sujoysarkar951@gmail.com>
2021-01-30 21:09:40 +0530

    u

--- Add.java
+++ Add.java
@@ -1,13 +1,21 @@
 import java.util.*;
+import java.io.*;
 public class Add{
         public static void main(String[] args){
         int a,b,sum;
+        PrintWriter pw=new PrintWriter(System.out,true);
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter two numbers : ");
+        // System.out.println("Enter two numbers : ");
+        
+        pw.println("Enter two numbers : ");
+        
         a=sc.nextInt();
         b=sc.nextInt();
         sum=a+b;
-        System.out.println("Sum of "+ a + " and " + b + " is " + sum);
+        // System.out.println("Sum of "+ a + " and " + b + " is " + sum);
+        
+        pw.println("Sum of "+ a + " and " + b + " is " + sum);
+        
         sc.close();
     }
 }
